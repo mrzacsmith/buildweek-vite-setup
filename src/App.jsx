b2235a39e35ae7bf9c25a421ffede8485b2ff8be
Zac Smith <zrs3141592@gmail.com>
2024-07-23 15:31:13 -0600

    👍 IMPROVE:

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='max-w-xl'>
+      <h1 className='text-4xl font-light'>started</h1>
+      <p className='font-light'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto debitis, iste quo nobis
+        accusamus unde atque optio nemo at deleniti assumenda molestias voluptatum dolorem quae
+        ratione aperiam dicta repellat voluptatibus! Quaerat facere cum reiciendis quidem deserunt
+        porro aspernatur. Iusto saepe sunt corrupti, illum quae mollitia id repellendus fuga
+        voluptate placeat sapiente harum vel distinctio obcaecati esse culpa! Recusandae, sunt
+        reprehenderit. Voluptas quibusdam ad optio modi cumque distinctio odio commodi sapiente
+        facere, praesentium vero voluptatem, fuga odit quaerat, mollitia quasi. Pariatur mollitia
+        cumque cupiditate quibusdam at exercitationem obcaecati commodi cum? Autem.
+      </p>
+    </div>
+  )
+}
+
+export default App
